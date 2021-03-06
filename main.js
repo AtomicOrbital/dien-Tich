fb9064178af7af5a10bc29a662a2641d47ca625f
thanhdat <topthuthuat69@gmail.com>
2021-03-07 01:53:29 +0700

    Nhúng file

---
+++ main.js
@@ -0,0 +1,4 @@
+let inputWidth=prompt("Enter the Width");
+       let inputHeight=prompt("Enter the Height");
+       let area = parseFloat(inputWidth) * parseFloat(inputHeight);
+       document.write("The area is : " + area);
